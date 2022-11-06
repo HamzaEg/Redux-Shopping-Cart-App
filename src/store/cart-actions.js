3553ec157be02362950083948217f2c0f75070aa
Hamza <h.egnaidi@gmail.com>
2022-11-06 20:29:42 +0100

    fix url firebase

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchData = () => {
   return async (dispatch) => {
     const fetchHandler = async () => {
       const res = await fetch(
-        "https://redux-http-bc782-default-rtdb.firebaseio.com/cartItems.json"
+        "https://redux-http-14859-default-rtdb.firebaseio.com/cartItems.json"
       );
       const data = await res.json();
       return data;
@@ -39,7 +39,7 @@ export const sendCartData = (cart) => {
       // Send state as Sending request
 
       const res = await fetch(
-        "https://redux-http-bc782-default-rtdb.firebaseio.com/cartItems.json",
+        "https://redux-http-14859-default-rtdb.firebaseio.com/cartItems.json",
         {
           method: "PUT",
           body: JSON.stringify(cart),
